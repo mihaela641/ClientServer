1389f7983f5c92db747280c49b7c90a5beb5c571
Mihaela <mihaela.dimitrova_@abv.bg>
2022-01-26 11:57:12 +0200

    add client and server logic

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,7 +1,74 @@
 package Client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Client {
+    private DataInputStream din;
+    private DataOutputStream dout;
+    private Socket socket;
     public static void main(String[] args) {
-        System.out.println("Hello");
+        new Client();
+    }
+
+    public Client() {
+        try {
+            socket = new Socket("localhost", 3333);
+            din = new DataInputStream(socket.getInputStream());
+            dout = new DataOutputStream(socket.getOutputStream());
+
+            listenForInput();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void listenForInput() {
+        Scanner console = new Scanner(System.in);
+        while (true) {
+            while (!console.hasNextLine()) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            String input = console.nextLine();
+
+            if (input.toLowerCase(Locale.ROOT).equals("quit")){
+                break;
+            }
+
+            try {
+                dout.writeUTF(input);
+
+                while (din.available() == 0){  //nothing available to read
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                String reply = din.readUTF();
+                System.out.println(reply);
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+
+        try {
+            din.close();
+            dout.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,4 +1,62 @@
 package Server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    ServerSocket serverSocket;
+    Socket socket;
+    DataInputStream din;
+    DataOutputStream dout;
+    public static void main(String[] args) {
+        new Server();
+    }
+
+    public Server(){
+        try {
+            serverSocket = new ServerSocket(3333);
+            socket = serverSocket.accept();
+            din = new DataInputStream(socket.getInputStream());
+            dout = new DataOutputStream(socket.getOutputStream());
+
+            listenForData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void listenForData() {
+        while (true) {
+            try {
+                while (din.available() == 0) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                String dataIn = din.readUTF();
+
+                dout.writeUTF(dataIn);
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+
+        }
+
+        try {
+            din.close();
+            dout.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
